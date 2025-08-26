71b898fea2d8bbab29d1776ba2cf5d7bb3d79536
badhon-cse <2221081073@uttarauniversity.edu.bd>
2025-08-26 11:54:35 +0600

    added functions for code reuseability

--- home.js
+++ home.js
@@ -1,16 +1,48 @@
+const validPin = 775900;
+
+// function to get int value from input
+
+function getValueInt(id) {
+  const intValue = parseInt(document.getElementById(id).value);
+  return intValue;
+}
+
+// function to get string value from input
+
+function getValue(id) {
+  const value = document.getElementById(id).value;
+  return value;
+}
+
+// function to get innertextvalue to int
+
+function getInnerText(id) {
+  const innerText = parseInt(document.getElementById(id).innerText);
+  return innerText;
+}
+
+// function to toggle
+
+function handleToggle(id) {
+  const forms = document.getElementsByClassName("forms");
+  for (const form of forms) {
+    form.style.display = "none";
+  }
+  document.getElementById(id).style.display = "block";
+}
+
+// add money features
+
 document
   .getElementById("add-money-button")
   .addEventListener("click", function (e) {
     e.preventDefault();
-    const selectedBank = document.getElementById("bank").value;
-    const accountnumber = parseInt(
-      document.getElementById("account-number").value
-    );
-    const addAmount = parseInt(document.getElementById("add-amount").value);
-    const pin = parseInt(document.getElementById("add-pin").value);
-    const validPin = 775900;
 
-    const balance = parseInt(document.getElementById("balance").innerText);
+    const selectedBank = getValue("bank");
+    const accountnumber = getValueInt("account-number");
+    const addAmount = getValueInt("add-amount");
+    const pin = getValueInt("add-pin");
+    const balance = getInnerText("balance");
 
     if (pin !== validPin && accountnumber.length != 11) {
       alert("Wrong Credentials");
@@ -21,24 +53,24 @@ document
     document.getElementById("balance").innerText = updateBalance;
   });
 
+// Toggle between add mmoney and cashout
+
 document.getElementById("add-money").addEventListener("click", function () {
-  document.getElementById("add-money-section").style.display = "block";
-  document.getElementById("cashout-section").style.display = "none";
+  handleToggle("add-money-section");
 });
 
 document.getElementById("cashout").addEventListener("click", function () {
-  document.getElementById("add-money-section").style.display = "none";
-  document.getElementById("cashout-section").style.display = "block";
+  handleToggle("cashout-section");
 });
+
+// Withdraw money features
 
 document
   .getElementById("withdraw-money-button")
   .addEventListener("click", function (e) {
     e.preventDefault();
-    const withdrawAmount = parseInt(
-      document.getElementById("withdraw-amount").value
-    );
-    const balance = parseInt(document.getElementById("balance").innerText);
+    const withdrawAmount = getValueInt("withdraw-amount");
+    const balance = getInnerText("balance");
     updateBalance = balance - withdrawAmount;
     document.getElementById("balance").innerText = updateBalance;
   });
