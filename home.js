40a250f9852e8975a3a969eb03f50b938daf4009
badhon-cse <2221081073@uttarauniversity.edu.bd>
2025-08-26 09:19:30 +0600

    Cashout functionalities done

--- home.js
+++ home.js
@@ -19,5 +19,26 @@ document
 
     const updateBalance = balance + addAmount;
     document.getElementById("balance").innerText = updateBalance;
-    console.log(updateBalance);
+  });
+
+document.getElementById("add-money").addEventListener("click", function () {
+  document.getElementById("add-money-section").style.display = "block";
+  document.getElementById("cashout-section").style.display = "none";
+});
+
+document.getElementById("cashout").addEventListener("click", function () {
+  document.getElementById("add-money-section").style.display = "none";
+  document.getElementById("cashout-section").style.display = "block";
+});
+
+document
+  .getElementById("withdraw-money-button")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const withdrawAmount = parseInt(
+      document.getElementById("withdraw-amount").value
+    );
+    const balance = parseInt(document.getElementById("balance").innerText);
+    updateBalance = balance - withdrawAmount;
+    document.getElementById("balance").innerText = updateBalance;
   });
